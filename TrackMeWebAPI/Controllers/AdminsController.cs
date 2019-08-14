790fbeb91f2cc1aa701efbc4921c87aa8c11f3d7
Bartosz Grochal <radu480@o2.pl>
2019-08-14 21:17:53 +0200

    Added Admins controller with GET method.

---
+++ TrackMeWebAPI/Controllers/AdminsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TrackMeWebAPI.DAL;
+using TrackMeWebAPI.ViewModels;
+
+namespace TrackMeWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "BasicUser")]
+    public class AdminsController : ControllerBase
+    {
+        private readonly DatabaseContext databaseContext;
+
+        public AdminsController(DatabaseContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+
+        // GET api/admins
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AdminViewModel>>> GetAdmins()
+        {
+            return await this.databaseContext.Admins
+                .Select(x => new AdminViewModel
+                {
+                    ID = x.ID,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                }).ToListAsync();
+
+        }
+
+    }
+}
