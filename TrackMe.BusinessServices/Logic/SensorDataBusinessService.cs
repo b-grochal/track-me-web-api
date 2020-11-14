08080d97d23999a16bee2dfd16a1509c11ef63c1
Bartosz <radu480@o2.pl>
2020-11-14 23:49:33 +0100

    Created empty services.

---
+++ TrackMe.BusinessServices/Logic/SensorDataBusinessService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackMe.BusinessServices.Logic
+{
+    class SensorDataBusinessService
+    {
+    }
+}
---
+++ TrackMe.Services/Logic/TripsService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackMe.Services.Logic
+{
+    class TripsService
+    {
+    }
+}
