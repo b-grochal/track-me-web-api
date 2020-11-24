e8d4d3a15efd4c78c53c8a972f1b52bb62535a2f
Bartosz <radu480@o2.pl>
2020-11-24 21:16:34 +0100

    Installed Automapper library.

---
+++ TrackMe.Models/Profiles/AuthProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TrackMe.Domain.Entities;
+using TrackMe.Models.DTOs.Auth;
+
+namespace TrackMe.Models.Profiles
+{
+    public class AuthProfile : Profile
+    {
+        public AuthProfile()
+        {
+            CreateMap<RegistrationDto, BasicUser>();
+        }
+    }
+}
