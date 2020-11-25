7616e5a7b8bce4572812e48ace6d52268459a34d
Bartosz <radu480@o2.pl>
2020-11-25 17:36:50 +0100

    Configured mappings profiles for basic users.

--- TrackMe.Models/Profiles/BasicUsersProfile.cs
+++ TrackMe.Models/Profiles/BasicUsersProfile.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TrackMe.Domain.Entities;
+using TrackMe.Models.DTOs.BasicUsers;
 
 namespace TrackMe.Models.Profiles
 {
-    class BasicUsersProfile
+    public class BasicUsersProfile : Profile
     {
+        public BasicUsersProfile()
+        {
+            CreateMap<BasicUser, BasicUserDto>()
+                .ForMember(dest => dest.BasicUserId, opts => opts.MapFrom(src => src.Id));
+        }
     }
 }
