8ac5cf0cc1eff19d00ba7207be8fcacfdf7d49fe
Bartosz <radu480@o2.pl>
2020-11-25 15:06:42 +0100

    Implemented business service for basic users.

--- TrackMe.BusinessServices/Interfaces/IBasicUsersBusinessService.cs
+++ TrackMe.BusinessServices/Interfaces/IBasicUsersBusinessService.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using TrackMe.Models.DTOs.BasicUsers;
 
 namespace TrackMe.BusinessServices.Interfaces
 {
-    interface IBasicUsersBusinessService
+    public interface IBasicUsersBusinessService
     {
+        Task<IEnumerable<BasicUserDto>> GetBasicUsers();
+        Task<BasicUserDto> GetBasicUser(string basicUserId);
+        Task DeleteBasicUser(string basicUserId);
     }
 }
--- TrackMe.BusinessServices/Logic/BasicUsersBusinessService.cs
+++ TrackMe.BusinessServices/Logic/BasicUsersBusinessService.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using TrackMe.BusinessServices.Interfaces;
+using TrackMe.Models.DTOs.BasicUsers;
+using TrackMe.Services.Interfaces;
 
 namespace TrackMe.BusinessServices.Logic
 {
-    public class BasicUsersBusinessService
+    public class BasicUsersBusinessService : IBasicUsersBusinessService
     {
+        private readonly IMapper mapper;
+        private readonly IBasicUsersService basicUsersService;
+
+        public async Task DeleteBasicUser(string basicUserId)
+        {
+            await basicUsersService.DeleteBasicUser(basicUserId);
+        }
+
+        public async Task<BasicUserDto> GetBasicUser(string basicUserId)
+        {
+            var basicUser = await basicUsersService.GetBasicUser(basicUserId);
+            return mapper.Map<BasicUserDto>(basicUser);
+        }
+
+        public async Task<IEnumerable<BasicUserDto>> GetBasicUsers()
+        {
+            var basicUsers = await basicUsersService.GetBasicUsers();
+            return mapper.Map<IEnumerable<BasicUserDto>>(basicUsers);
+        }
     }
 }
