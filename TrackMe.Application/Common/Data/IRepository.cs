0616f6e119331ac2f85b87f692ab48aa5ec8fd35
Bartosz Grochal <radu480@outlook.com>
2025-05-17 22:37:16 +0200

    Prepare interface for generic repository.

---
+++ TrackMe.Application/Common/Data/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Common.Data;
+
+public interface IRepository<TEntity>
+{
+    Task InsertAsync(TEntity entity);
+
+    Task UpdateAsync(TEntity entity);
+
+    Task DeleteAsync(TEntity entity);
+
+    Task<TEntity> GetByIdAsync(int id);
+
+    Task<IEnumerable<TEntity>> GetAllAsync();
+}
