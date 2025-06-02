603c94cce14196fdffab5cc11d57a1924be4bde4
Bartosz Grochal <radu480@outlook.com>
2025-06-02 17:24:43 +0200

    Add basic stuff for architecture testing.

---
+++ ArchitectureTests/Common/BaseTest.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+using System.Reflection;
+
+namespace ArchitectureTests.Common
+{
+    public abstract class BaseTest
+    {
+        protected static readonly Assembly DomainAssembly = typeof(Entity).Assembly;
+        protected static readonly Assembly ApplicationAssembly = typeof(Application.DependencyInjection).Assembly;
+        protected static readonly Assembly InfrastructureAssembly = typeof(Infrastructure.DependencyInjection).Assembly;
+        protected static readonly Assembly WebApiAssembly = typeof(WebApi.DependencyInjection).Assembly;
+    }
+}
---
+++ WebApi/DependencyInjection.cs
@@ -0,0 +1,5 @@
+﻿namespace WebApi;
+
+public static class DependencyInjection
+{
+}
