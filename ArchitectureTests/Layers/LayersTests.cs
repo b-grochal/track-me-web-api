b413e6ce67bee5c52c80b38239bb7b73ed27da4c
Bartosz Grochal <radu480@outlook.com>
2025-06-02 18:01:20 +0200

    Prepare base architecture tests for clean architecture structure.

---
+++ ArchitectureTests/Layers/LayersTests.cs
@@ -0,0 +1,74 @@
+﻿using ArchitectureTests.Common;
+using Shouldly;
+using NetArchTest.Rules;
+
+namespace ArchitectureTests.Layers;
+
+public class LayersTests : BaseTest
+{
+    [Fact]
+    public void DomainLayer_Should_NotHaveDependencyOnApplicationLayer()
+    {
+        TestResult result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(ApplicationAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void DomainLayer_Should_NotHaveDependencyOnInfrastructureLayer()
+    {
+        TestResult result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void DomainLayer_Should_NotHaveDependencyOnWebApiLayer()
+    {
+        TestResult result = Types.InAssembly(DomainAssembly)
+            .Should()
+            .NotHaveDependencyOn(WebApiAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void ApplicationLayer_Should_NotHaveDependencyOnInfrastructureLayer()
+    {
+        TestResult result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(InfrastructureAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void ApplicationLayer_Should_NotHaveDependencyOnWebApiLayer()
+    {
+        TestResult result = Types.InAssembly(ApplicationAssembly)
+            .Should()
+            .NotHaveDependencyOn(WebApiAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void InfrastructureLayer_Should_NotHaveDependencyOnWebApiLayer()
+    {
+        TestResult result = Types.InAssembly(InfrastructureAssembly)
+            .Should()
+            .NotHaveDependencyOn(WebApiAssembly.GetName().Name)
+            .GetResult();
+
+        result.IsSuccessful.ShouldBeTrue();
+    }
+}
