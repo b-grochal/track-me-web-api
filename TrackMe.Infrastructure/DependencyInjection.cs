de45cfeb1989b5ece41692a44e4c53b189d386f6
Bartosz Grochal <radu480@outlook.com>
2025-06-25 21:59:09 +0200

    Add connection to postgres db.

--- TrackMe.Infrastructure/DependencyInjection.cs
+++ TrackMe.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,29 @@
-﻿namespace Infrastructure;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TrackMe.Database.Context;
+
+namespace Infrastructure;
 
 public static class DependencyInjection
 {
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration) =>
+        services
+            .AddDatabase(configuration);
+
+    private static IServiceCollection AddDatabase(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        string? connectionString = configuration.GetConnectionString("Database");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+
+        return services;
+    }
 }
