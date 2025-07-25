d89bfdf916e65f910b67944b8e4ffb3c5b4bdc29
Bartosz Grochal <radu480@outlook.com>
2025-07-25 22:57:22 +0200

    Adjust login handler with primary constructors.

---
+++ TrackMe.Application/Authentication/Common/AuthenticationErrors.cs
@@ -0,0 +1,11 @@
+﻿using Common.Errors;
+
+namespace Application.Authentication.Common;
+
+public static class AuthenticationErrors
+{
+    public static Error ApplicationUserNotFoundByEmail(string email) =>
+        Error.NotFound(
+            "Authentication.ApplicationUserNotFoundByEmail",
+            $"Application user with email '{email}' was not found.");
+}
--- TrackMe.Application/Authentication/Login/LoginCommandHandler.cs
+++ TrackMe.Application/Authentication/Login/LoginCommandHandler.cs
@@ -1,9 +1,11 @@
-﻿using Application.Common.Messaging;
+﻿using Application.Common.Data;
+using Application.Common.Messaging;
 using Common.Results;
 
 namespace Application.Authentication.Login
 {
-    internal sealed class LoginCommandHandler : ICommandHandler<LoginCommand, string>
+    internal sealed class LoginCommandHandler(IApplicationDbContext dbContext)
+        : ICommandHandler<LoginCommand, string>
     {
         public async Task<Result<string>> Handle(LoginCommand command, CancellationToken cancellationToken)
         {
