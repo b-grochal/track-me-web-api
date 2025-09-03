b4c093beac1023076e6461ec45c750601301c31d
Bartosz Grochal <radu480@outlook.com>
2025-09-03 13:41:30 +0200

    Prepare first unit test for login handler.

---
+++ Application.UnitTests/Authentication/LoginCommandHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿using Application.Authentication.Common;
+using Application.Authentication.Login;
+using Application.Common.Authentication;
+using Application.Common.Data;
+using Application.UnitTests.Mocks;
+using Common.Results;
+using Moq;
+using Shouldly;
+
+namespace Application.UnitTests.Authentication;
+public class LoginCommandHandlerTests
+{
+    private static readonly LoginCommand Command = new("test@member.com", "P@ssw0rd123#");
+
+    private readonly LoginCommandHandler _handler;
+    private readonly Mock<IApplicationDbContext> _dbContextMock = new();
+    private readonly Mock<IPasswordHasher> _passwordHasherMock = new();
+    private readonly Mock<IJwtProvider> _jwtProviderMock = new();
+
+
+    public LoginCommandHandlerTests()
+    {
+        _dbContextMock = ApplicationDbContextMockBuilder.Get();
+        _passwordHasherMock = new();
+        _jwtProviderMock = new();
+
+        _handler = new LoginCommandHandler(
+            _dbContextMock.Object,
+            _passwordHasherMock.Object,
+            _jwtProviderMock.Object);
+    }
+
+    [Fact]
+    public async void Handle_Should_ReturnError_WhenUserNotFound()
+    {
+        // Arrange
+        LoginCommand invalidCommand = Command with { Email = "test2@member.com" };
+
+        // Act
+        Result result = await _handler.Handle(invalidCommand, CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.ShouldBeFalse();
+        result.Error.ShouldBe(AuthenticationErrors.ApplicationUserNotFoundByEmail("test2@member.com"));
+    }
+}
---
+++ Application.UnitTests/Mocks/ApplicationDbContextMockBuilder.cs
@@ -0,0 +1,38 @@
+﻿using Application.Common.Data;
+using Domain.Admins;
+using Domain.ApplicationUsers;
+using Domain.Members;
+using MockQueryable.Moq;
+using Moq;
+
+namespace Application.UnitTests.Mocks;
+public static class ApplicationDbContextMockBuilder
+{
+    public static Mock<IApplicationDbContext> Get()
+    {
+        var applicationsUsers = new List<ApplicationUser>
+        {
+            new Admin
+            {
+                Id = 1,
+                FirstName = "Test",
+                LastName = "Admin",
+                Email = "test@admin.com"
+            },
+            new Member
+            {
+                Id = 2,
+                FirstName = "Test",
+                LastName = "Member",
+                Email = "test@member.com"
+            }
+        };
+
+        var applicationUsersDbSetMock = applicationsUsers.BuildMockDbSet();
+
+        var applicationDbContextMock = new Mock<IApplicationDbContext>();
+        applicationDbContextMock.Setup(db => db.ApplicationUsers).Returns(applicationUsersDbSetMock.Object);
+
+        return applicationDbContextMock;
+    }
+}
